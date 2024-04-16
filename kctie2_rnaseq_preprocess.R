23917c50610c9b9a168420e68d32b581044da073
smithit <isabelle.t.smith@vanderbilt.edu>
2024-04-16 10:41:40 -0500

    Removed scripts folder, added samples metadata df in .Rmd

---
+++ kctie2_rnaseq_preprocess.R
@@ -0,0 +1,11 @@
+## Transforming raw RNA-seq data
+
+## Installing necessary packages
+install.packages()
+
+## loading necessary packages
+library(tidyverse)
+library()
+
+## load data
+counts <- read.csv("data/2024_04_10/kctie2_rnaseq_counts.csv")
